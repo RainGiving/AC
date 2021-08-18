f0eebe3cae94ec4dcdb9b0e7d5ea663b01fcde9f
RainGiving <raingiving@gmail.com>
2021-08-18 15:40:42 +0800

    update

---
+++ Acwing_Basic/code/AcWing790_ 数的三次方根.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+const double eps = 1e-8;
+
+double n;
+
+
+double bsearch(double l, double r)
+{
+    while(eps < r - l)
+    {
+        double mid = (l + r) / 2;
+        if(mid * mid * mid < n) l = mid;
+        else                    r = mid;
+    }
+    return l;
+}
+
+
+int main()
+{
+    cin >> n;
+    printf("%.6f", bsearch(-10000, 10000));
+    return 0;
+}
